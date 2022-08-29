95f2a0a074668c60f86ac44b547997d9c647bdfd
Alba Rico <amiloyrico.36@gmail.com>
2022-08-29 18:36:17 -0500

    Initial Commit

---
+++ Ejercicio1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio1
+//
+//  Created by Alba Yolima Rico Montenegro on 29/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
